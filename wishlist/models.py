310fbd90bde02954d4c829d3770d3dbcec8e126b
Anshid <muhammedanshid173@gmail.com>
2026-06-22 17:18:54 +0530

    Add whislist model

--- wishlist/models.py
+++ wishlist/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
+from django.conf import settings
+
 # Create your models here.
+class Wishlist(models.Model):
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='wishlist'
+    )
+    product = models.ForeignKey('products.Product', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return f"{self.user.email} - {self.product.name}"
+    class Meta:
+        unique_together = ('user', 'product')
+        ordering = ['-created_at']
